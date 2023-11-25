64154aac60d22ea752fa58c07077559af769383f
Nico53175 <nsambeth52@gmail.com>
2023-11-25 10:51:44 +0100

    Item SO

---
+++ Assets/Scripts/Entities/Items/ItemSO.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Item", menuName = "Item", order = 1)]
+public class ItemSO : ScriptableObject
+{
+    public string itemName;
+    public int itemID;
+    public Sprite itemSprite;
+}
